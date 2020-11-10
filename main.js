2dd522063601179eed44c4dae771d68c51e897a6
Kevin Floyd <kfloydy@gmail.com>
2020-11-09 19:33:38 -0600

    removed border

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-let inputField = document.getElementById('inputField');
+//Create variables for the field, button and list container IDs
+let inputField = document.getElementById('inputField'); 
 let toDoButton = document.getElementById('todobutton');
 let toDoList = document.getElementById('toDoList');
 
+//Make a click event listener
 toDoButton.addEventListener('click', function(){
-    var paragraph = document.createElement('p');
+    // Make a variable to create a p element
+    var paragraph = document.createElement('p'); 
+    //Adds list class to the paragraph
     paragraph.classList.add('list');
+    //Adds the actual variable to the list container
     toDoList.appendChild(paragraph);
+    //Adds the text entered in the text box
     paragraph.innerText = inputField.value;
+    //Resets the input field value
     inputField.value =" ";
+    //Makes a line-through if clicked once
     paragraph.addEventListener('click', function(){
         paragraph.style.textDecoration = "line-through";
     })
+    //Deletes it if clicked twice
     paragraph.addEventListener('dblclick', function(){
         toDoList.removeChild(paragraph);
     })
